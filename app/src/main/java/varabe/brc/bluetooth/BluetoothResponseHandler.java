ab529e87b1db523cdd1840bc35d0f8ce150d4ce7
varabe <zertarx@gmail.com>
2018-08-10 23:56:02 +0300

    Fix Message codes not being avallible to DeviceConnector

--- app/src/main/java/varabe/brc/bluetooth/BluetoothResponseHandler.java
+++ app/src/main/java/varabe/brc/bluetooth/BluetoothResponseHandler.java
@@ -15,11 +15,11 @@ public class BluetoothResponseHandler extends Handler {
     private WeakReference<MainActivity> mActivity;
 
     // Message types sent from the DeviceConnector Handler
-    private static final int MESSAGE_STATE_CHANGE = 1;
-    private static final int MESSAGE_READ = 2;
-    private static final int MESSAGE_WRITE = 3;
-    private static final int MESSAGE_DEVICE_NAME = 4;
-    private static final int MESSAGE_TOAST = 5;
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
 
     // Messages that inform user of current connection state
     public static String MESSAGE_NOT_CONNECTED;
